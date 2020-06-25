7b36626304ba60b470f402d47c712d854dfd96d8
Mun94 <seongwoo720@gmail.com>
2020-06-25 15:32:32 +0900

    200625 | DONE | 데이터 연동

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -1,24 +1,8 @@
-import React, {useState} from 'react';
-import axios from 'axios';
+import React from 'react';
+import NewsList from './components/NewsList.js'
 
 const App = () => {
-  const [data, setData] = useState(null);
-  const onClick = async() => {
-    try{
-      const response = await axios.get("http://newsapi.org/v2/top-headlines?country=kr&apiKey=0745e578210c45a893e907fde4457947")
-      setData(response);
-    }catch(e){
-      console.log(e)
-    }
-  };
-  return (
-    <div>
-      <div>
-        <button onClick = {onClick}>불러오기</button>
-      </div>
-        {data && <textarea rows={7} value={JSON.stringify(data,null,2)}/>}
-    </div>
-  )
+  return <NewsList/>;
 }
 
 export default App;
---
+++ news-viewer/src/components/NewsList.js
@@ -0,0 +1,56 @@
+import React, {useState, useEffect} from "react";
+import styled from "styled-components";
+import NewsItem from './NewsItem.js';
+import axios from 'axios';
+
+const NewsListBlock = styled.div`
+    box-sizing: border-box;
+    padding-bottom: 3rem;
+    width : 768px;
+    margin : 0 auto;
+    margin-top : 2rem;
+    @media screen and (max-width: 768px){
+        width:100%;
+        padding-left: 1rem;
+        padding-right : 1rem;
+    }
+`;
+
+const NewsList = () => {
+    const [articles, setArticles] = useState(null);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        const fetchData = async() => {
+            setLoading(true);
+            try{
+                const response = await axios.get("http://newsapi.org/v2/top-headlines?country=kr&apiKey=0745e578210c45a893e907fde4457947");
+                setArticles(response.data.articles);
+            }catch(e){
+                console.log(e)
+            }
+            setLoading(false);
+        };
+        fetchData();
+    },[]);
+
+    if(loading){
+        return <NewsListBlock>Loading..</NewsListBlock>
+    }
+
+    if(!articles){
+        return null;
+    }
+
+    return(
+        <NewsListBlock>
+            {
+                articles.map(article => (
+                    <NewsItem key={article.url} article={article}/>
+                ))
+            }
+        </NewsListBlock>
+    )
+}
+
+export default NewsList;
