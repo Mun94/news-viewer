00a92d8e47b9532d876229b973a6aace0f1bc8f1
Mun94 <seongwoo720@gmail.com>
2020-06-26 19:47:09 +0900

    Updates

---
+++ precache-manifest.37430425d1aaf614a3533bc557d2b7dc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86adc56a8034083a2d2a2240c3f7fd5d",
+    "url": "/news-viewer/index.html"
+  },
+  {
+    "revision": "803572465e961afa9e7f",
+    "url": "/news-viewer/static/js/2.fed90979.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/news-viewer/static/js/2.fed90979.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0bf1ab0905ac9eaed42e",
+    "url": "/news-viewer/static/js/main.07839cc2.chunk.js"
+  },
+  {
+    "revision": "ef96a0caf70099cc53ce",
+    "url": "/news-viewer/static/js/runtime-main.cdcd38ea.js"
+  }
+]);
